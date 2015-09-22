12cd8def0b4d95520abc544f1507f8c8e9053b57
Gibran Arias <gibran.arias@mexicoder.com.mx>
2015-09-22 16:14:05 -0500

    Update index to display images, this is not the final style

--- angular/controllers.js
+++ angular/controllers.js
@@ -1,5 +1,5 @@
 app.controller('imageSearcherController', ['$scope', 'imageSearcherService', function($scope, imageSearcherService) {
   imageSearcherService.success(function(data){
-    $scope.data1 = data;
+    $scope.images = data;
   });
 }]);
