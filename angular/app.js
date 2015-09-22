34f4ff7fd68c2baa4696c10cdfc5e2e72f4caf76
Gibran Arias <gibran.arias@mexicoder.com.mx>
2015-09-22 12:24:04 -0500

    Create app and controller just to print a hello world

---
+++ angular/app.js
@@ -0,0 +1,6 @@
+(function(){
+    angular.module(
+        'ImageSearcherApp',
+        ['ImageSearcherController']
+    );
+})();
---
+++ angular/controllers.js
@@ -0,0 +1,7 @@
+(function(){
+    'use strict';
+    angular.module('controllers', [])
+        .controller('wikipediaController', ['$scope', function($scope){
+            $scope.hola = "hola";
+        }]);
+})();
