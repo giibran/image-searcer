428288921846022692517797b6fe4497cbedc8c4
Gibran Arias <gibran.arias@mexicoder.com.mx>
2015-09-24 09:19:08 -0500

    Add search behavior

--- angular/services.js
+++ angular/services.js
@@ -1,5 +1,7 @@
 app.factory('imageSearcherService', ['$http', function($http){
-    return $http.get('https://pixabay.com/api/?username=giibran&key=3069db9066b179dd206e&q=yellow+flower&image_type=photo').success(function(data){
+    var search = location.search;
+    search = search.split("?search=");
+    return $http.get('https://pixabay.com/api/?username=giibran&key=3069db9066b179dd206e&q='+ search[1] +'&image_type=photo').success(function(data){
     return data;
   })
   .error(function(err){
